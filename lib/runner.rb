cdcb94924410118f6784acc4b2571226dd5221cb
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-06-26 14:32:12 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,7 +1,18 @@
 require_relative "blackjack.rb"
-
 def runner
+  # code runner here
   welcome
+  total = initial_round
+  until total > 21
+  
+  total = hit?(total)
+  
+  
 end
+end_game(total)
+  
+  
+end
+    
 
-runner
+    runner
